dc28553f0d88719db19271212223b3258f902c52
Lokesh S <lokeshlucky13579@gmail.com>
2023-02-27 19:37:15 +0530

    reusing custom widget

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -16,9 +16,53 @@ class _InputPageState extends State<InputPage> {
           'BMI Calculator',
         ),
       ),
-      body: const Center(
-        child: Text(
-          'Body Text',
+      body: Column(
+        children: [
+          Expanded(
+            child: Row(
+              children: const [
+                ReusableCard(
+                  colour: Color(0xFF1D1E33),
+                ),
+                ReusableCard(
+                  colour: Color(0xFF1D1E33),
+                ),
+              ],
+            ),
+          ),
+          const ReusableCard(
+            colour: Color(0xFF1D1E33),
+          ),
+          Expanded(
+            child: Row(
+              children: const [
+                ReusableCard(
+                  colour: Color(0xFF1D1E33),
+                ),
+                ReusableCard(
+                  colour: Color(0xFF1D1E33),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class ReusableCard extends StatelessWidget {
+  final Color colour;
+  const ReusableCard({super.key, required this.colour});
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Container(
+        margin: const EdgeInsets.all(15.0),
+        decoration: BoxDecoration(
+          color: colour,
+          borderRadius: BorderRadius.circular(10),
         ),
       ),
     );
