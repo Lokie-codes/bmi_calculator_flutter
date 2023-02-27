c93e9db8cbf4c15b3deeb275a5afc6ffeadf1d3d
Lokesh S <lokeshlucky13579@gmail.com>
2023-02-27 11:10:35 +0530

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const BMICalculator());
+
+class BMICalculator extends StatelessWidget {
+  const BMICalculator({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.dark().copyWith(
+        colorScheme: ColorScheme.fromSwatch().copyWith(
+          primary: const Color(0xFF0A0E21),
+          secondary: Colors.teal,
+        ),
+        scaffoldBackgroundColor: const Color(0xFF0A0E21),
+      ),
+      home: const InputPage(),
+    );
+  }
+}
+
+class InputPage extends StatefulWidget {
+  const InputPage({super.key});
+
+  @override
+  State<InputPage> createState() => _InputPageState();
+}
+
+class _InputPageState extends State<InputPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'BMI Calculator',
+        ),
+      ),
+      body: const Center(
+        child: Text(
+          'Body Text',
+        ),
+      ),
+    );
+  }
+}
