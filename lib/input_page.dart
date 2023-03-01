68106b787eaf8b6a28154ea7e8441757d276ae5c
Lokesh S <lokeshlucky13579@gmail.com>
2023-03-01 13:34:04 +0530

    fnuctionality of gender cards (buttons)

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -5,7 +5,10 @@ import 'reusable_card.dart';
 
 const bottomContainerHeight = 80.0;
 const activeCardColor = Color(0xFF1D1E33);
+const inactiveCardColor = Color(0xFF111328);
 const bottomContainerColor = Color(0xFFEB1555);
+
+enum GenderType { male, female, none }
 
 class InputPage extends StatefulWidget {
   const InputPage({super.key});
@@ -15,6 +18,22 @@ class InputPage extends StatefulWidget {
 }
 
 class _InputPageState extends State<InputPage> {
+  GenderType selectedGender = GenderType.none;
+  // Color maleCardColor = inactiveCardColor;
+  // Color femaleCardColor = inactiveCardColor;
+
+  // void updateColor(GenderType gender) {
+  //   gender == GenderType.male ? maleCardColor = activeCardColor : maleCardColor = inactiveCardColor;
+  //   gender == GenderType.female ? femaleCardColor = activeCardColor : femaleCardColor = inactiveCardColor;
+  //   // if (gender == GenderType.male) {
+  //   //   maleCardColor = activeCardColor;
+  //   //   femaleCardColor = inactiveCardColor;
+  //   // } else {
+  //   //   femaleCardColor = activeCardColor;
+  //   //   maleCardColor = inactiveCardColor;
+  //   // }
+  // }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -27,19 +46,37 @@ class _InputPageState extends State<InputPage> {
         children: [
           Expanded(
             child: Row(
-              children: const [
-                ReusableCard(
-                  colour: activeCardColor,
-                  cardChild: IconContent(
-                    label: 'Male',
-                    icon: FontAwesomeIcons.mars,
+              children: [
+                Expanded(
+                  child: GestureDetector(
+                    onTap: () => setState(() {
+                      selectedGender = GenderType.male;
+                    }),
+                    child: ReusableCard(
+                      colour: selectedGender == GenderType.male
+                          ? activeCardColor
+                          : inactiveCardColor,
+                      cardChild: const IconContent(
+                        label: 'Male',
+                        icon: FontAwesomeIcons.mars,
+                      ),
+                    ),
                   ),
                 ),
-                ReusableCard(
-                  colour: activeCardColor,
-                  cardChild: IconContent(
-                    label: 'Female',
-                    icon: FontAwesomeIcons.venus,
+                Expanded(
+                  child: GestureDetector(
+                    onTap: () => setState(() {
+                      selectedGender = GenderType.female;
+                    }),
+                    child: ReusableCard(
+                      colour: selectedGender == GenderType.female
+                          ? activeCardColor
+                          : inactiveCardColor,
+                      cardChild: const IconContent(
+                        label: 'Female',
+                        icon: FontAwesomeIcons.venus,
+                      ),
+                    ),
                   ),
                 ),
               ],
@@ -48,11 +85,13 @@ class _InputPageState extends State<InputPage> {
           Expanded(
             child: Row(
               children: const [
-                ReusableCard(
-                  colour: activeCardColor,
-                  cardChild: IconContent(
-                    label: 'Card 3',
-                    icon: FontAwesomeIcons.earthEurope,
+                Expanded(
+                  child: ReusableCard(
+                    colour: activeCardColor,
+                    cardChild: IconContent(
+                      label: 'Card 3',
+                      icon: FontAwesomeIcons.earthEurope,
+                    ),
                   ),
                 ),
               ],
@@ -61,18 +100,22 @@ class _InputPageState extends State<InputPage> {
           Expanded(
             child: Row(
               children: const [
-                ReusableCard(
-                  colour: activeCardColor,
-                  cardChild: IconContent(
-                    label: 'Card 4',
-                    icon: FontAwesomeIcons.earthAsia,
+                Expanded(
+                  child: ReusableCard(
+                    colour: activeCardColor,
+                    cardChild: IconContent(
+                      label: 'Card 4',
+                      icon: FontAwesomeIcons.earthAsia,
+                    ),
                   ),
                 ),
-                ReusableCard(
-                  colour: activeCardColor,
-                  cardChild: IconContent(
-                    label: 'Card 5',
-                    icon: FontAwesomeIcons.earthAmericas,
+                Expanded(
+                  child: ReusableCard(
+                    colour: activeCardColor,
+                    cardChild: IconContent(
+                      label: 'Card 5',
+                      icon: FontAwesomeIcons.earthAmericas,
+                    ),
                   ),
                 ),
               ],
